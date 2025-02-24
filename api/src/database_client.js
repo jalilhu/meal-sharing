cf7266ae31e3e64c627058d4f67a04672f3b37ad
jalilhu <jhus12b@gmail.com>
2025-02-24 15:31:20 +0100

    edited and updated

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -1,15 +1,23 @@
 import knex from "knex";
 
+console.log("DB Connection Details:", {
+  client: process.env.DB_CLIENT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE_NAME,
+  ssl: process.env.DB_USE_SSL,
+});
+
 const connection = knex({
   client: process.env.DB_CLIENT,
   connection: {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT), // Ensure port is a number
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
-    ssl:
-      process.env.DB_USE_SSL === "true" ? { rejectUnauthorized: false } : false,
+    ssl: { rejectUnauthorized: false },
   },
 });
 
